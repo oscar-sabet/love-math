ad76d76a06356a352f241992ba8ecbd6b605fefc
Oscar Sabet <osabet021@gmail.com>
2025-01-29 16:07:01 +0000

    blahblah

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,33 @@ document.addEventListener('DOMContentLoaded', function () {
         alert('You clicked Submit!')
       } else {
         let gameType = this.getAttribute('data-type')
-        alert(`You clicked ${gameType}`)
+        runGame(gameType)
       }
     })
   }
+  runGame('addition')
 })
-
-function runGame () {
+/**
+ * The Main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ * @[gameType] {string} gameType - The type of game being played
+ */
+function runGame (gameType) {
   let num1 = Math.floor(Math.random() * 25) + 1
   let num2 = Math.floor(Math.random() * 25) + 1
+
+  if (gameType === 'addition') {
+    displayAdditionQuestion(num1, num2)
+  } else {
+    alert(`Unknown game type: ${gameType}`)
+    throw `Unknown game type: ${gameType}. Aborting!`
+  }
 }
+
+/**
+ *
+ *
+ */
 function checkAnswer () {
   pass
 }
@@ -29,8 +46,10 @@ function incrementScore () {
 function incrementWrongAnswer () {
   pass
 }
-function displayAdditionQuestion () {
-  pass
+function displayAdditionQuestion (operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1
+  document.getElementById('operand2').textContent = operand2
+  document.getElementById('operator').textContent = '+'
 }
 function displaySubtractQuestion () {
   pass
